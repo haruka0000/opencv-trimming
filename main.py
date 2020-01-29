3eae877689b3e139ec76ed5cb5d812d498f09a8f
Haruka Takahashi <h173335@edu.tut.ac.jp>
2020-01-29 19:27:33 +0900

    adding face_detection

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import os
 import numpy as np
 import cv2
 from pycocotools.coco import COCO
+import dlib
 
 data_dir    = 'data/coco/'
 ann_file    = 'annotations/instances_train2017.json'
 img_dir     = 'images/train2017'
-output_dir  = 'data/coco_edge'
+output_dir  = 'data/samples'
 
 
 def edge(img):
@@ -61,43 +62,82 @@ def deleteBG(img_data):
 
 
 
-def replaceCharcter(img_data):
+def checkHuman(img_data):
     ann_ids     = coco.getAnnIds(imgIds=img_data['id'], iscrowd=None)
     anns        = coco.loadAnns(ann_ids)
-    cats        = coco.loadCats(coco.getCatIds())
     
-    # print(cats)
-    for ann in anns:
-        cat = [ cat for cat in cats if cat['id'] == ann["category_id"] ][0]
-        if cat["supercategory"] == "person":
-            for seg in ann['bbox']:
-                print( seg, " ", end="" )
+    human_anns  = []
 
-            ann_width   = ann['bbox'][2] - ann['bbox'][0]
-            ann_height  = ann['bbox'][3] - ann['bbox'][1]
-            c_x         = ann['bbox'][0] + ann_width / 2
-            c_y         = ann['bbox'][1] + ann_height / 2
+    for ann in anns:
+        cat = [ cat for cat in cats if cat['id'] == ann["category_id"] ][0] # このアノテーションのスーパーカテゴリ
+        if cat["supercategory"] == "person":
+            human_anns.append(ann)
+    
+    if human_anns != []:
+        return human_anns
+    else:
+        return None
+
+
+def replaceCharcter(org_img, human_anns, mode="replace"):
+    org_img = org_img.copy()
+
+    for ann in human_anns:
+        cat = [ cat for cat in cats if cat['id'] == ann["category_id"] ][0] # このアノテーションのスーパーカテゴリ
+        
+        if cat["supercategory"] == "person":
+            for seg in ann['segmentation']:
+                if type(seg) == list:   # 多人数の場合は別形式で扱われるため
+                    poly = np.array(seg).reshape((int(len(seg)/2), 2)).reshape((-1,1,2)).astype(np.int32)
+                    
+                    # 白埋め
+                    cv2.fillPoly(org_img, pts=[poly], color=(255, 255, 255))
             
-            print( "c_x=", c_x, "\t c_y=", c_y)
-            print( "width=", ann_width, "\t heigth=", ann_height )
+            if mode == "replace":
+                ann_width   = ann['bbox'][2] - ann['bbox'][0]
+                ann_height  = ann['bbox'][3] - ann['bbox'][1]
+                c_x         = ann['bbox'][0] + ann_width / 2
+                c_y         = ann['bbox'][1] + ann_height / 2
+
+
+            # print( "c_x=", c_x, "\t c_y=", c_y)
+            # print( "width=", ann_width, "\t heigth=", ann_height )
+    return org_img
+
+
         
 
 
 if __name__ == "__main__":
     coco    = COCO(os.path.join(data_dir, ann_file))
+    cats        = coco.loadCats(coco.getCatIds())
+
     image_files = sorted(os.listdir(os.path.join(data_dir, img_dir)))
 
-    img_ids     = [int(img_file.split('.')[0]) for img_file in image_files]
+    # img_ids     = [int(img_file.split('.')[0]) for img_file in image_files]
+    img_ids     = [int(img_file.split('.')[0]) for img_file in image_files][0:20]   # To develoment
 
     for idx, img_id in enumerate(img_ids):
-        # img_data    = coco.loadImgs(img_ids[np.random.randint(0,len(img_ids))])[0]
+        # img_data    = coco.loadImgs(img_ids[np.random.randint(0,len(img_ids))])[0]     # To develoment
         img_data    = coco.loadImgs(img_id)[0]
         
         # 画像の読み込み
         org_img = cv2.imread(os.path.join(data_dir, img_dir, img_data['file_name']))
 
+        """
+        if checkHuman( img_data ):
+            human_anns   = checkHuman( img_data )
+            output_img  = replaceCharcter( org_img, human_anns )
+        else: 
+            output_img  = org_img
+            print(" No Human. ")
+        """
+        output_img  = org_img
+        detector = dlib.get_frontal_face_detector()
+        faces = detector(output_img, 0)
+        print(faces)
         # cutout_img = deleteBG(img_data)
-        output_img = edge(org_img)
+        # output_img = edge(org_img)
 
         # output_img = binarization(cutout_img)
         # cv2.namedWindow('org')
@@ -109,9 +149,11 @@ if __name__ == "__main__":
         # cv2.imshow('original', org_img)
         # cv2.imwrite('original.jpg', org_img)
 
-        # cv2.namedWindow('output')
-        # cv2.imshow('output', output_img)
-        cv2.imwrite(os.path.join(output_dir, str(img_id).zfill(12) + ".jpg"), output_img)
+        cv2.namedWindow('output')
+        cv2.imshow('output', output_img)
+
+        # cv2.imwrite(os.path.join(output_dir, str(img_id).zfill(12) + ".jpg"), output_img)
         print(str(idx+1).rjust(20), "/", len(img_ids))
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
